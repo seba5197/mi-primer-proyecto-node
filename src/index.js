3ec4aa7be72a1f6e0b4f9801e8d805acb857b71b
seba5197 <sebastian5197@gmail.com>
2022-03-27 20:10:49 -0300

    nueva mod

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hellow word");
+console.log('hellow word')
+const express = require('express')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hola mundo')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
