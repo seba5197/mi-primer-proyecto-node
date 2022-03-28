975be7e731284411bf71b97c3fab9a49021325ec
seba5197 <sebastian5197@gmail.com>
2022-03-27 23:56:20 -0300

    tarea ciisa

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-console.log('hellow word')
-const express = require('express')
 
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const router = require('./router')
 const PORT = process.env.PORT || 3000
 const app = express()
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.use(morgan('dev'))
+app.use(router)
 app.get('/', (req, res) => {
-  res.send('Hola mundo')
+  res.render('home')
 })
+
+console.log(path.join(__dirname, 'views'))
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
---
+++ src/router.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.render('home')
+})
+
+router.get('/about', (req, res) => {
+  res.render('about')
+})
+
+router.get('*', (req, res) => {
+  res.render('404')
+})
+
+module.exports = router
